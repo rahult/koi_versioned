fc7e1d708907e1ced66b11b176e514f10160779d
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-07 23:41:44 +1030

    Added basic draft storage capabilities

--- lib/koi_versioned/is_versioned.rb
+++ lib/koi_versioned/is_versioned.rb
@@ -8,11 +8,16 @@ module KoiVersioned
     module ClassMethods
       def is_versioned
         before_create :set_version_state
+        serialize :version_draft, Hash
       end
     end
 
+    def ignore_columns
+      ["id", "version_state", "version_draft", "created_at", "updated_at"]
+    end
+
     def set_version_state
-      self.version_state = false unless version_state
+      self.version_state = false if version_state.nil?
       true
     end
 
@@ -28,6 +33,30 @@ module KoiVersioned
       self.version_state = true
       self.version_draft = nil
       save
+    end
+
+    def draft!
+      # Only proceed if record has changed and already has a published version
+      return true if !changed? && !is_published?
+
+      # Store all attributes temporarily skipping read only attributes
+      draft = attributes.reject { |key, value| ignore_columns.include? key }
+
+      # Realod attributes from the database to clear all dirty attributes
+      reload
+
+      # Save draft in version_draft
+      self.version_draft = draft
+
+      # Temporarily disable time stamp updates as we do not want to record draft updated_at time
+      self.class.record_timestamps = false
+      begin
+        # Saves the record
+        save
+      ensure
+        # Re-enable time stamp updates
+        self.class.record_timestamps = true
+      end
     end
   end
 end
--- test/dummy/test/unit/post_test.rb
+++ test/dummy/test/unit/post_test.rb
@@ -6,11 +6,10 @@ class PostTest < ActiveSupport::TestCase
   end
 
   test "should create new post" do
-    assert @post.save
+    assert Factory(:post).save
   end
 
   test "should save new post in draft state by default" do
-    @post.save
     assert @post.is_draft?
     assert !@post.is_published?
   end
@@ -28,9 +27,26 @@ class PostTest < ActiveSupport::TestCase
   end
 
   test "should have the ability to publish" do
-    @post.save
     assert @post.is_draft?
     @post.publish!
     assert @post.is_published?
+  end
+
+  test "should update post attributes with draft after publish!" do
+    changed_title = @post.title.reverse
+    @post.title = changed_title
+    @post.publish!
+    assert @post.is_published?
+    assert_equal changed_title, @post.title
+  end
+
+  test "should keep original post attributes after saving draft" do
+    original_title = @post.title
+    original_updated_at = @post.updated_at
+    changed_title = original_title.reverse
+    @post.title = changed_title
+    @post.draft!
+    assert_equal original_title, @post.title
+    assert_equal original_updated_at, @post.updated_at
   end
 end
